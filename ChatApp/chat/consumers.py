b36faa9a4210aa12f60cc0430ae0475917316074
GDI <dimon.1982@mail.ru>
2023-03-12 18:58:58 +0700

    changed to sync WebSocket

--- ChatApp/chat/consumers.py
+++ ChatApp/chat/consumers.py
@@ -1,41 +1,42 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
 
     # Receive message from WebSocket
-    async def receive(self, text_data=None, bytes_data=None):
+    def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = text_data_json['username']
 
         # send message to room group
-        await self.channel_layer.group_send(self.room_group_name, {
+        async_to_sync(self.channel_layer.group_send)(self.room_group_name, {
             'type': 'chat_message',
             'message': message,
             'username': username,
         })
 
     # Receive message from room group
-    async def chat_message(self, event):
+    def chat_message(self, event):
         message = event['message']
         username = event['username']
 
         # send message to WebSocket
-        await self.send(text_data=json.dumps({'message': message, 'username': username}))
+        self.send(text_data=json.dumps({'message': message, 'username': username}))
 
         

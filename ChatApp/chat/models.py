b592ffdf374e8dfd66cbcdf98f258ff6160291ca
GDI <dimon.1982@mail.ru>
2023-03-12 18:05:33 +0700

    accidentally deleted chatapp then ctrl+z action

--- ChatApp/chat/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class ChatUser(AbstractUser):
-    phone = models.IntegerField(default=789)
-    # class Meta:
-    #   verbose_name_plural = 'ChatUsers'
